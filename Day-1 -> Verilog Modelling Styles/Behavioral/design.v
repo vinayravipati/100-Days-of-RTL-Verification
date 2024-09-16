3a4ac924c79e55ca7cbb83140b36a6cb06b90903
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-16 19:00:04 +0530

    Update design.v

--- Day-1 -> Verilog Modelling Styles/Behavioral/design.v
+++ Day-1 -> Verilog Modelling Styles/Behavioral/design.v
@@ -1,1 +1,17 @@
-
+/*
+Behavioral modeling in Verilog describes the logic and functionality of a design using high-level constructs like always blocks.
+*/
+module logic_gates(
+  input wire a,b;
+  output reg and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate;
+);
+  always @(*) begin
+    and_gate = a&b;
+    or_gate = a|b;
+    not_gate = ~a;
+    nand_gate = ~(a&b);
+    nor_gate = ~(a|b);
+    xor_gate = a^b;
+    xnor_gate = ~(a^b);
+  end
+endmodule
