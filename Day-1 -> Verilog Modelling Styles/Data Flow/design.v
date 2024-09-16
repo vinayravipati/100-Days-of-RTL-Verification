303645a5d79fb9b9fa8bde07b6e690c836bc0bfe
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-16 19:09:23 +0530

    Update design.v

--- Day-1 -> Verilog Modelling Styles/Data Flow/design.v
+++ Day-1 -> Verilog Modelling Styles/Data Flow/design.v
@@ -1,1 +1,14 @@
-
+/*
+Dataflow modeling in Verilog uses continuous assignment statements (assign) to describe the circuit's behavior using logical 
+expressions, representing the flow of data through combinational logic directly.
+*/
+module logic_gates(
+  input wire a,b;
+  output reg and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate;
+);
+  assign and_gate = a&b;
+  assign or_gate = a|b;
+  assign not_gate = ~a;
+  assign nand_gate = ~(a&b);
+  assign nor_gate = ~(a|b);
+  assign and_gate = a&b;
