3767feddd5df2f0d1aa2f277d9af2cc2c4bb2b4b
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-16 19:41:20 +0530

    Update testbench.v

--- Day-1 -> Verilog Modelling Styles/Behavioral/testbench.v
+++ Day-1 -> Verilog Modelling Styles/Behavioral/testbench.v
@@ -1,1 +1,15 @@
+`include "design.v"
+module logic_gates_tb(
+  input wire and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate;
+  output reg a,b;
+);
+  logic_gates inst(.a(a), .b(b), .and_gate(and_gate), .or_gate(or_gate), .not_gate(not_gate), .nand_gate(nand_gate), .nor_gate(nor_gate), .xor_gate(xor_gate), .xnor_gate(xnor_gate));
 
+  initial begin
+    $monitor("Time=%0t a=%b b=%b and_gate=%b or_gate=%b not_gate=%b nand_gate=%b nor_gate=%b xor_gate=%b xnor_gate=%b",$time,a,b,and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate);
+    #1 a=1'b0;b=1'b0;
+    #1 a=1'b0;b=1'b1;
+    #1 a=1'b1;b=1'b0;
+    #1 a=1'b1;b=1'b1;
+  end
+endmodule
