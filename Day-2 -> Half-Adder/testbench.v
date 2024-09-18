a0c3d1c41b6e12eddaed000353ef3e0584069f71
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-18 14:35:07 +0530

    Update testbench.v

--- Day-2 -> Half-Adder/testbench.v
+++ Day-2 -> Half-Adder/testbench.v
@@ -1,1 +1,11 @@
-
+`include "halfadder.v"
+module testbench(output reg a,b,input wire sum,carry);
+  halfadder dut(a,b,sum,carry);
+  initial begin
+    $monitor("Time=%t a=%b b=%b sum=%b carry=%b",$time,a,b,sum,carry);
+    #1 a=0;b=0;
+    #1 a=0;b=1;
+    #1 a=1;b=0;
+    #1 a=1;b=1;
+  end
+endmodule
