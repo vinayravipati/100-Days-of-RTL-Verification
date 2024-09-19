bd659ece52713fcc6c1ca456563dade04144ee09
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-19 16:25:00 +0530

    Update design.v

--- Day-3 -> Full Adder/Using HA_FA/design.v
+++ Day-3 -> Full Adder/Using HA_FA/design.v
@@ -1,1 +1,20 @@
+module half_adder(
+    input wire a, b,
+    output reg sum, carry
+);
+always @* begin
+    sum = a ^ b;
+    carry = a & b;
+end
+endmodule
+
+module full_adder(
+    input wire a, b, c_in,
+    output reg sum, carry_out
+);
+wire s1, c1, c2;
+half_adder ha1(a, b, s1, c1);
+half_adder ha2(s1, c_in, sum, c2);
+assign carry_out = c1 | c2;
+endmodule
 
