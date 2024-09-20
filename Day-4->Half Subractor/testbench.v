3a76268609c76bc4364476a236a7c0770090440d
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-20 17:29:28 +0530

    Update testbench.v

--- Day-4->Half Subractor/testbench.v
+++ Day-4->Half Subractor/testbench.v
@@ -4,7 +4,7 @@ output reg a,b;
 input wire d0,b0;
   half_sub inst(.d0 (d0), b0(b0), .a(a), .b(b));
   initial begin
-    $monitor("Time=%0 => a=%b b=%b difference=%b borrow=%b",$time,a,b,d0,b0);
+    $monitor("Time=%0t => a=%b b=%b difference=%b borrow=%b",$time,a,b,d0,b0);
       #10 a=0;b=0;
       #10 a=0;b=1;
       #10 a=1;b=0;
