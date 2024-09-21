765541a3c3a4552c0915b7ae321adc996ed47224
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-21 16:46:07 +0530

    Update testbench.v

--- Day-5->Full Subtractor/testbench.v
+++ Day-5->Full Subtractor/testbench.v
@@ -1,1 +1,17 @@
-
+module full_sub_half_tb(a,b,cin,difference,borrow);
+  input wire difference,borrow;
+  output reg a,b,cin;
+  full_sub dut(a,b,cin,difference,borrow);
+  initial begin
+    $monitor("Time=%0t a=%0b b=%0b cin=%0b difference=%0b borrow=%0b",$time,a,b,cin,difference,borrow);
+    #5 a=0;b=0;cin=0;
+    #5 a=0;b=0;cin=1;
+    #5 a=0;b=1;cin=0;
+    #5 a=0;b=1;cin=1;
+    #5 a=1;b=0;cin=0;
+    #5 a=1;b=0;cin=1;
+    #5 a=1;b=1;cin=0;
+    #5 a=1;b=1;cin=1;
+    #5;
+  end
+endmodule
