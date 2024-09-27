5abeccfadee6b3abe0712449ff6d95ae50dc054f
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-27 19:34:20 +0530

    Update design.v

--- Day-11->decoder3X8/design.v
+++ Day-11->decoder3X8/design.v
@@ -1,1 +1,14 @@
-
+module decoder_2x4(a,b,en,y);
+  input wire a,b,en;
+  output reg [3:0]y;
+  assign y[0]=(a&(~b)&~en);
+  assign y[1]=(a&(~b&en));
+  assign y[2]=(a&b&(~en));
+  assign y[3]=(a&b&en);
+endmodule
+module decoder_3x8(a,b,c,y);
+  input wire a,b,c;
+  output reg [7:0]y;
+  decoder_2x4 n1(~a,b,(c),y[3:0]);
+  decoder_2x4 n2(a,b,c,y[7:4]);
+endmodule
