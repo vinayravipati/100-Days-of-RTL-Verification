27b1913c03e66d39ca1262d487c79a91796c0667
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-09-30 21:09:42 +0530

    Delete Day-13 -> 3x8 Priority Encoder directory

--- Day-13 -> 3x8 Priority Encoder/design.v
+++
@@ -1,7 +0,0 @@
-module priorityencoder8x3 (d, bo,b1,b2);
-input [7:0]d;
-output b0,b1,b2;
-assign b2=d[7]|[d[6]| [d[5]|d[4];
-assign bl=d[7]|d[6]| (~d[5]&~d[4]&d[3]) |(~d[5]& ~d[4]|d [2]);
-assign bo=d[7]|((~d[6])&d [5])|(~d[6])&(~d[4])&d[3])|((~d[6])&(~d[4])&(~d[2])&d[1]);
-endmodule
