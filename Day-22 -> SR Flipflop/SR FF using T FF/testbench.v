044c62568289459c31547e5269f6a1b1a4151fda
vinayravipati <57246817+vinayravipati@users.noreply.github.com>
2024-10-15 22:11:47 +0530

    Create testbench.v

---
+++ Day-22 -> SR Flipflop/SR FF using T FF/testbench.v
@@ -0,0 +1,19 @@
+`include "srff_using_tff.v"
+module srff_using_tff_test;
+  reg s,r,clk;
+  wire q;
+  srff_using_tff inst(.s(s), .r(r), .clk(clk), .q(q));
+  initial begin
+    clk=0;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    $monitor("time=%t s=%b r=%b clk=%b q=%b",$time,s,r,clk,q);
+    #10 s=0;r=0;
+    #10 s=0;r=1;
+    #10 s=1;r=0;
+    #10 s=1;r=1;
+    #10;
+    $finish;
+  end
+endmodule
